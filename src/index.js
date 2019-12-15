b211aef2b3be07acd2e3cc4cfc9d26a778afd39d
Alan Johnson <nalanj@fastmail.com>
2019-12-15 17:13:04 -0500

    Adds ability to specify database url as arg
    Also cleans up error handling

--- src/index.js
+++ src/index.js
@@ -5,9 +5,16 @@ const yargs = require("yargs");
 const { Client } = require("pg");
 const fs = require("fs");
 
-const options = yargs.usage("Usage: $0 <script> [args]").demandCommand(1).argv;
+const options = yargs
+  .usage("Usage: $0 [options] <script> [args]")
+  .demandCommand(1)
+  .string("d")
+  .alias("d", "databaseUrl")
+  .describe("d", "Specify the database url").argv;
+
 const script = options._[0];
 const args = options._.slice(1);
+const dbUrl = options.databaseUrl || process.env.DATABASE_URL;
 
 const client = new Client({ connectionString: process.env.DATABASE_URL });
 client.connect(err => {
@@ -17,14 +24,21 @@ client.connect(err => {
   }
 });
 
-var query = fs.readFileSync(script, "utf8");
+try {
+  var query = fs.readFileSync(script, "utf8");
+} catch (err) {
+  console.error("Error", err.message);
+  process.exit(1);
+}
 
 client.query(query, args, (err, res) => {
-  if (err) {
-    console.error("Error", err.message);
-  } else {
+  if (!err) {
     console.log(JSON.stringify(res.rows, null, 2));
   }
-
   client.end();
+
+  if (err) {
+    console.error("Error", err.message);
+    process.exit(1);
+  }
 });

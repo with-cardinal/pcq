3f9bed120773577a5f93ddfe1adc974e20c9edd7
Alan Johnson <nalanj@fastmail.com>
2019-12-25 12:54:25 -0500

    Added colored output
    Also gets ready for running multiple scripts

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const yargs = require("yargs");
 const { Client } = require("pg");
 const fs = require("fs");
+const chalk = require("chalk");
 
 const options = yargs
   .usage("Usage: $0 [options] <script> [args]")
@@ -16,29 +17,43 @@ const script = options._[0];
 const args = options._.slice(1);
 const dbUrl = options.databaseUrl || process.env.DATABASE_URL;
 
-const client = new Client({ connectionString: process.env.DATABASE_URL });
-client.connect(err => {
-  if (err) {
-    console.error("Error", err.message);
-    process.exit();
-  }
-});
+const scripts = [script];
 
-try {
-  var query = fs.readFileSync(script, "utf8");
-} catch (err) {
-  console.error("Error", err.message);
-  process.exit(1);
-}
+(async () => {
+  const client = new Client({ connectionString: process.env.DATABASE_URL });
+  client.connect(err => {
+    if (err) {
+      console.error(chalk.red("-- Error"), err.message);
+      process.exit();
+    }
+  });
 
-client.query(query, args, (err, res) => {
-  if (!err) {
-    console.log(JSON.stringify(res.rows, null, 2));
+  let counter = 0;
+  for (const file of scripts) {
+    console.error(chalk.green("-- Start %s"), file);
+
+    try {
+      const query = fs.readFileSync(script, "utf8");
+      const res = await client.query(query, args);
+
+      if (res.rows) {
+        console.log(JSON.stringify(res.rows, null, 2));
+      }
+      console.error(chalk.green("-- Finish"), script);
+    } catch (err) {
+      console.error(chalk.red("-- Error"), err.message);
+      client.end();
+      console.error(chalk.red("-- Exiting early due to error"));
+      process.exit(1);
+    }
+
+    counter++;
   }
+
   client.end();
-
-  if (err) {
-    console.error("Error", err.message);
-    process.exit(1);
-  }
-});
+  console.error(
+    chalk.green("-- Ran %s script%s"),
+    counter,
+    counter == 1 ? "" : "s"
+  );
+})();
